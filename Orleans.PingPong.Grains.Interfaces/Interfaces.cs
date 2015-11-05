4f0921b5ff117200ebc828f0c08404be52782b13
Vadim Obradovich <vadik.on@gmail.com>
2015-11-06 00:38:04 +0300

    Update to Orleans 1.0.10

--- Orleans.PingPong.Grains.Interfaces/Interfaces.cs
+++ Orleans.PingPong.Grains.Interfaces/Interfaces.cs
@@ -6,25 +6,25 @@ using Orleans.Concurrency;
 
 namespace Orleans.PingPong
 {
-    [Immutable]
-    public class Message
-    {}
+	[Immutable]
+	public class Message
+	{}
 
-    public interface IClient : IGrainWithGuidKey
+	public interface IClient : IGrainWithGuidKey
 	{
-        Task Run();
-        Task Pong(IDestination from, Message message);
-        Task Initialize(IDestination actor, long repeats);
-        Task Subscribe(IClientObserver subscriber);
-    }
+		Task Run();
+		Task Pong(IDestination from, Message message);
+		Task Initialize(IDestination actor, long repeats);
+		Task Subscribe(IClientObserver subscriber);
+	}
 
-    public interface IClientObserver : IGrainObserver
-    {
-        void Done(long pings, long pongs);
-    }
-
-    public interface IDestination : IGrainWithGuidKey
+	public interface IClientObserver : IGrainObserver
 	{
-        Task Ping(IClient from, Message message);
-    }
+		void Done(long pings, long pongs);
+	}
+
+	public interface IDestination : IGrainWithGuidKey
+	{
+		Task Ping(IClient from, Message message);
+	}
 }
--- Orleans.PingPong.Grains/Implementations.cs
+++ Orleans.PingPong.Grains/Implementations.cs
@@ -6,69 +6,69 @@ using Orleans.Concurrency;
 
 namespace Orleans.PingPong
 {
-    public class Client : Grain, IClient
-    {
-        static readonly Message msg = new Message();
-
-        IDestination actor;
-        ObserverSubscriptionManager<IClientObserver> subscribers;
-
-        long pings;
-        long pongs;
-        long repeats;
-
-        public override Task OnActivateAsync()
-        {
-            subscribers = new ObserverSubscriptionManager<IClientObserver>();
-            return TaskDone.Done;
-        }
-
-        public Task Initialize(IDestination actor, long repeats)
-        {
-            this.actor = actor;
-            this.repeats = repeats;
-
-            return TaskDone.Done;
-        }
-
-        public Task Run()
-        {
-            actor.Ping(this, msg).Ignore();
-            pings++;
-
-            return TaskDone.Done;
-        }
-
-        public Task Pong(IDestination @from, Message message)
-        {
-            pongs++;
-
-            if (pings < repeats)
-            {
-                actor.Ping(this, msg);
-                pings++;
-            }
-            else if (pongs >= repeats)
-            {
-                subscribers.Notify(x => x.Done(pings, pongs));
-            }
-
-            return TaskDone.Done;
-        }
-
-        public Task Subscribe(IClientObserver subscriber)
-        {
-            subscribers.Subscribe(subscriber);
-            return TaskDone.Done;
-        }
-    }
-
-    public class Destination : Grain, IDestination
+	public class Client : Grain, IClient
 	{
-        public Task Ping(IClient @from, Message message)
-        {
-            from.Pong(this, message).Ignore();
-            return TaskDone.Done;
-        }
-    }
+		static readonly Message msg = new Message();
+
+		IDestination actor;
+		ObserverSubscriptionManager<IClientObserver> subscribers;
+
+		long pings;
+		long pongs;
+		long repeats;
+
+		public override Task OnActivateAsync()
+		{
+			subscribers = new ObserverSubscriptionManager<IClientObserver>();
+			return TaskDone.Done;
+		}
+
+		public Task Initialize(IDestination actor, long repeats)
+		{
+			this.actor = actor;
+			this.repeats = repeats;
+
+			return TaskDone.Done;
+		}
+
+		public Task Run()
+		{
+			actor.Ping(this, msg).Ignore();
+			pings++;
+
+			return TaskDone.Done;
+		}
+
+		public Task Pong(IDestination @from, Message message)
+		{
+			pongs++;
+
+			if (pings < repeats)
+			{
+				actor.Ping(this, msg);
+				pings++;
+			}
+			else if (pongs >= repeats)
+			{
+				subscribers.Notify(x => x.Done(pings, pongs));
+			}
+
+			return TaskDone.Done;
+		}
+
+		public Task Subscribe(IClientObserver subscriber)
+		{
+			subscribers.Subscribe(subscriber);
+			return TaskDone.Done;
+		}
+	}
+
+	public class Destination : Grain, IDestination
+	{
+		public Task Ping(IClient @from, Message message)
+		{
+			from.Pong(this, message).Ignore();
+			return TaskDone.Done;
+		}
+	}
 }
